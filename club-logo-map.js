15366e9726d4255a4bb11b95b0ade4a68f5cca68
soylevimx-hue <soy.levi.mx@gmail.com>
2026-04-01 02:27:53 -0600

    Update club-logo-map.js - 2026-04-01T02:27:52.639189

---
+++ club-logo-map.js
@@ -0,0 +1,15 @@
+const clubLogoMap = {
+    "tigres": 8561,
+    "america": 6576,
+    "genoa": 10233,
+    "ac milan": 8564,
+    "real madrid": 8633,
+    "real betis": 8603,
+    "liverpool": 8650,
+    "chivas": 7807,
+    "feyenoord": 10235,
+    "manchester city": 8456,
+    "nashville": 915807,
+    "sc braga": 10264,
+    "sc sporting braga": 10264
+};
